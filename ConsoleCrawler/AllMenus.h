8b1c9a142b2c4c41e47314d005eedf4d6a91128f
Buddelexperte <74837601+Buddelexperte@users.noreply.github.com>
2025-11-13 13:52:53 +0100

    deleted whole project -> new base coming...

--- ConsoleCrawler/AllMenus.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "MenuElement.h"
-#include "MainMenu.h"
-#include "EndScreen.h"
-#include "ControlsHint.h"
-#include "HUD.h"
--- ConsoleCrawler/Types.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-enum class EInputReturn
-{
-	CONSUMED = 0,
-	NOT_USED,
-	FAILED,
-	COUNT
-};
